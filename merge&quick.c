07ec922b71249e3a16f7a14011cfafbe83eb05b6
syan02 <147817020+syan02@users.noreply.github.com>
2025-03-24 20:34:55 +0530

    Update merge&quick.c

--- merge&quick.c
+++ merge&quick.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-// Merge Sort Functions
+// Function to merge two halves in Merge Sort
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1, n2 = right - mid;
     int L[n1], R[n2];
 
-    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
 
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+    while (i < n1 && j < n2)
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    
+    while (i < n1)
+        arr[k++] = L[i++];
+    
+    while (j < n2)
+        arr[k++] = R[j++];
 }
 
+// Merge Sort function
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
@@ -25,9 +32,11 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
-// Quick Sort Functions
+// Function to partition array for Quick Sort
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high], i = low - 1;
+    int pivot = arr[high];
+    int i = low - 1;
+    
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
@@ -36,12 +45,15 @@ int partition(int arr[], int low, int high) {
             arr[j] = temp;
         }
     }
+    
     int temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
+    
     return i + 1;
 }
 
+// Quick Sort function
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
@@ -50,52 +62,48 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
-// Function to print an array
+// Function to print array
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-// Main Function
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5, 3, 6, 2, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    // Copying the array to maintain original values
-    int arr1[n], arr2[n];
+    int n;
+    
+    // Getting input from the user
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n], arr1[n], arr2[n];
+    
+    printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
-        arr1[i] = arr[i];
-        arr2[i] = arr[i];
+        scanf("%d", &arr[i]);
+        arr1[i] = arr[i]; // Copy for Merge Sort
+        arr2[i] = arr[i]; // Copy for Quick Sort
     }
 
-    // Merge Sort Execution Time
+    // Measure Merge Sort execution time
     clock_t start_merge = clock();
     mergeSort(arr1, 0, n - 1);
     clock_t end_merge = clock();
     double time_merge = ((double)(end_merge - start_merge)) / CLOCKS_PER_SEC;
 
-    printf("Sorted array using Merge Sort:\n");
+    printf("\nSorted array using Merge Sort:\n");
     printArray(arr1, n);
-    printf("Time taken by Merge Sort: %f seconds\n\n", time_merge);
+    printf("Time taken by Merge Sort: %f seconds\n", time_merge);
 
-    // Quick Sort Execution Time
+    // Measure Quick Sort execution time
     clock_t start_quick = clock();
     quickSort(arr2, 0, n - 1);
     clock_t end_quick = clock();
     double time_quick = ((double)(end_quick - start_quick)) / CLOCKS_PER_SEC;
 
-    printf("Sorted array using Quick Sort:\n");
+    printf("\nSorted array using Quick Sort:\n");
     printArray(arr2, n);
     printf("Time taken by Quick Sort: %f seconds\n", time_quick);
 
     return 0;
 }
-
-/*Sorted array using Merge Sort:
-1 2 3 4 5 6 7 8 9 10
-Time taken by Merge Sort: 0.000003 seconds
-
-Sorted array using Quick Sort:
-1 2 3 4 5 6 7 8 9 10
-Time taken by Quick Sort: 0.000002 seconds*/
